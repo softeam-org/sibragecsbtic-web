0da4a85f798a104cfffe03f9221a7449813e30b9
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-18 14:34:37 -0300

    Frist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <Routes>
+        <Route path='/*' element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
