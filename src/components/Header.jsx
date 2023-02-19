8ba709411ad6b3d8b655aab731faaa5e170852af
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-19 17:03:55 -0300

    Header criado

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div>
+            <div>
+                LOGO
+            </div>
+            <div>
+               Sibragec XXIII 
+            </div>
+        </div>
+        <div>
+            <p>Início</p>
+            <p>Programação</p>
+            <p>Inscrições</p>
+            <p>Submissão</p>
+            <p>Local</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
