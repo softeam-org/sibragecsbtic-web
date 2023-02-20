c25880bcdbc359601a3a36c8ef299261ee480135
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-20 15:16:10 -0300

    Constantes passadas para arquivo

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+export const navLinks = [
+    {
+      name: "Início",
+      path: "/",
+      links: [{ name: "Sobre", path: "/sobre" }],
+    },
+    {
+      name: "Programação",
+      path: "/programacao",
+      links: [{ name: "Sobre", path: "/sobre" }],
+    },
+    {
+      name: "Inscrições",
+      path: "/inscricoes",
+      links: [{ name: "Sobre", path: "/sobre" }],
+    },
+    {
+      name: "Submissão",
+      path: "/submissao",
+      links: [{ name: "Sobre", path: "/sobre" }],
+    },
+    {
+      name: "Local",
+      path: "/local",
+      links: [
+        { name: "UFAL", path: "/local/ufal" },
+        { name: "Maceió", path: "/local/maceio" },
+        { name: "Hotel do evento", path: "/local/hotel-do-evento" },
+      ],
+    },
+  ];
