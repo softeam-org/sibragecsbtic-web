9956b402fa06992d237143f12cc8c9d20a740d39
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-20 14:37:38 -0300

    Added underline styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
 import logo from "../assets/logo-01.svg";
+import { useLocation } from "react-router-dom";
+
+
+const navLinks = [
+  { name: "Início", path: "/" },
+  { name: "Programação", path: "/programacao" },
+  { name: "Inscrições", path: "/inscricoes" },
+  { name: "Submissão", path: "/submissao" },
+  { name: "Local", path: "/local" },
+];
+
+
+
 
 function Header() {
+  const currentPath = useLocation();
+  
+
   return (
     <header className="bg-nord0">
       <div className="max-w-6xl mx-auto pt-14">
@@ -13,11 +29,17 @@ function Header() {
       <div className="flex flex-col items-center gap-16">
         <h1 className="text-7xl font-bold text-white">Sibragec XXIII</h1>
         <div className="flex gap-20 pb-12">
-          <a className="font-semibold text-xl">Início</a>
-          <a className="font-semibold text-xl">Programação</a>
-          <a className="font-semibold text-xl">Inscrições</a>
-          <a className="font-semibold text-xl">Submissão</a>
-          <a className="font-semibold text-xl">Local</a>
+          {
+            navLinks.map((link) => (
+              <a 
+                className={
+                  "navlink font-semibold text-xl" 
+                  + 
+                  (currentPath.pathname === link.path ? " active" : "")}
+              >
+                {link.name}
+              </a>
+          ))}
         </div>
       </div>
       </div>
