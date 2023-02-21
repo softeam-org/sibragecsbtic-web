3ec2e03b124a0dfc064fdec18247bb9ae4832d83
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-21 15:20:49 -0300

    Horários adicionados

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,19 +4,23 @@ import { dayData } from '../constants'
 
 function Schedule({ day }) {
   return (
-    <div>
-        <div>
+    <div className='max-w-6xl mx-auto'>
+        <div className='mb-6 py-4 border-b-2 border-b-nord2 text-nord1 text-xl font-bold'>
             {dayData[day].date}
         </div>
         <div>
-            {dayData[day].schedule.map((item, index) => (
-                <div>
-                    <div>{item.time}</div>
-                    <div>
-                        <p>{item.title}</p>
-                        <p>{item.description}</p>
-                        <div>
-                            {item.speakers.map((speaker, index) => <img src={speaker.photo} />)}
+            {dayData[day].schedule.map((item) => (
+                <div className='flex justify-between mb-6' key={item.title}>
+                    <div className=' text-nord1 font-bold'>{item.time}</div>
+                    <div className='max-w-3xl'>
+                        <p className='text-nord1 font-bold mb-4'>{item.title}</p>
+                        <p className='text-gray-500 font-semibold'>{item.description}</p>
+                        <div className='flex flex-row-reverse'>
+                            {item.speakers.map((speaker, index) => (
+                                <img 
+                                    src={speaker.photo}
+                                    className={`w-10 h-10 rounded-full relative object-cover object-top ${index > 0 ? 'left-2' : ''}`} 
+                                />))}
                         </div>
                     </div>
                 </div>
