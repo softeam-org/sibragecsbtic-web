a7e885bf946348377156df65aa4cc96e9446b069
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-21 15:22:43 -0300

    Constantes de template adicionadas

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+import Luciano_IFS from "./assets/images/Luciano_IFS.jpg";
+import Sergio from "./assets/images/sergio.jpg";
+
 export const navLinks = [
   {
     name: "Início",
@@ -45,3 +48,170 @@ export const navLinks = [
     ],
   },
 ];
+
+// Path: src\components\Navbar\index.js
+
+
+export const dayData = {
+  1: {
+    date: "Segunda, 30 de janeiro",
+    schedule: [
+      {
+        time: "8h:30 - 10h:00",
+        title: "Abertura",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elitiasty. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: Sergio,
+          },
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: Luciano_IFS,
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 2",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+    ]
+  },
+  2: {
+    date: "Segunda, 30 de janeiro",
+    schedule: [
+      {
+        time: "8h:30 - 10h:00",
+        title: "Abertura",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          },
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 2",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+    ]
+  },
+  3: {
+    date: "Segunda, 30 de janeiro",
+    schedule: [
+      {
+        time: "8h:30 - 10h:00",
+        title: "Abertura",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          },
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 2",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+      {
+        time: "10h:00 - 11h:30",
+        title: "Lorem ipsum dolor sit amet 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        speakers: [
+          {
+            name: "Lorem ipsum dolor sit amet",
+            photo: "https://picsum.photos/200/300",
+          }
+        ]
+      },
+    ]
+  }
+}
+
+// Path: src\pages\Programacao\index.js
