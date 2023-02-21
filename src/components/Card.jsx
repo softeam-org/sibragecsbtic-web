65b22e2bc4f52c98174dbcc10e48394adbbdcf1f
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-02-21 19:01:06 -0300

    Cor do Card mudada para branco e "srcset" removido

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Card({image,name,description,institution}){
     return(
-        <div className="bg-black rounded-lg shadow-md text-gray-800 px-8 mx-8" >
-            <img className="w-40 h-40 object-cover rounded-full" src={image} alt="" srcset=""></img>
+        <div className="bg-white rounded-lg shadow-md text-gray-800 px-8 mx-8" >
+            <img className="w-40 h-40 object-cover rounded-full" src={image} alt=""></img>
             <div className="p-4">
             <h3 className="text-lg font-semibold">{name}</h3>
             <p>{description}</p>
