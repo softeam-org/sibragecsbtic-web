192f9f8a4a87c3b677f15297f400919ae7997603
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-21 13:54:25 -0300

    Estrutura dos horários criada

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import { dayData } from '../constants'
+
 function Schedule({ day }) {
   return (
     <div>
-
+        <div>
+            {dayData[day].date}
+        </div>
+        <div>
+            {dayData[day].schedule.map((item, index) => (
+                <div>
+                    <div>{item.time}</div>
+                    <div>
+                        <p>{item.title}</p>
+                        <p>{item.description}</p>
+                        <div>
+                            {item.speakers.map((speaker, index) => <img src={speaker.photo} />)}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
