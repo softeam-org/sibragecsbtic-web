0134edeadd719a8a9c0b4f3f9e96b21cbf20070d
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-25 20:30:18 -0300

    Instrução adicionada

--- src/container/Submissao.jsx
+++ src/container/Submissao.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import Instructions from "../components/Instructions";
+import { articleInstructions } from "../constants";
+
+
 function Submissao() {
   return (
-    <div>Submissao</div>
+    <div className="bg-white">
+      <div className="w-100 bg-nord6 py-20">
+        <div className="max-w-6xl mx-auto">
+          <div className="mb-6 py-4 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
+            <h2>Artigos</h2>
+          </div>
+          <div className="w-100 flex justify-center mb-10 mt-8">
+        <h3 className=" text-nord1 text-2xl font-bold">COMO CARREGAR O ARQUIVO DO ARTIGO NO NOVO SISTEMA:</h3>
+      </div>
+          <Instructions data={articleInstructions} />
+
+          <div className="mb-6 py-4 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
+            <h2>Calendário</h2>
+          </div>
+          <div className="mb-6 py-4 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
+            <h2>Temáticas dos Artigos</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
