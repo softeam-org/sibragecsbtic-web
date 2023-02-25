84d41b9a7f30e68e46943dbb0631d3adac55f839
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-25 20:03:54 -0300

    Minicursos estilizado

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const coursesData = [
   {
     id: 1,
-    name: "Curso 1",
+    name: "Planejamento de Obras nos níveis de médio e curto prazo",
     image: "https://picsum.photos/200/300",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl.",
@@ -12,7 +12,7 @@ const coursesData = [
   },
   {
     id: 2,
-    name: "Curso 2",
+    name: "Planejamento de Obras nos níveis de médio e curto prazo",
     image: "https://picsum.photos/200/300",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl.",
@@ -21,7 +21,7 @@ const coursesData = [
   },
   {
     id: 3,
-    name: "Curso 3",
+    name: "Planejamento de Obras nos níveis de médio e curto prazo",
     image: "https://picsum.photos/200/300",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl. Sed euismod, nisl vitae ultricies ultricies, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl.",
@@ -32,7 +32,7 @@ const coursesData = [
 
 function Courses() {
   return (
-    <div className="bg-white py-20">
+    <div className="bg-gray-50 py-20">
       <div className="max-w-6xl mx-auto">
         <div className="mb-8">
           <h2 className="text-3xl font-bold text-gray-900">MiniCursos</h2>
@@ -49,14 +49,15 @@ function Courses() {
             eleifend consectetur. Ut viverra dui eros, eu commodo augue volutpat
             et.
           </p>
-          <div className="w-100 flex justify-center items-center">
+          <div className="w-100 flex justify-center items-center gap-12">
             {coursesData.map((course) => (
-              <div className="px-8 py-12 shadow-lg">
+              <div className="flex flex-col items-center justify-center px-9 py-9 gap-5 shadow-lg rounded-lg">
                 <img 
                 src={course.image}
-                className="w-64 h-44 object-cover" 
+                className="w-72 h-48 object-cover" 
                 alt="" />
-                <h3 className="text-lg text-nord0">{course.name}</h3>
+
+                <h3 className="text-lg text-nord0 font-bold">{course.name}</h3>
                 <p className="text-zinc-400">{course.presenter}</p>
                 <a href="">
                   <button className="bg-orange-500 border-0 text-white font-bold py-3 px-16  shadow-md hover:shadow-lg rounded-md hover:bg-orange-400 transition-all ease-in">
