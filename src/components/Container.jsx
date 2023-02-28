8ba2d6f4a0d627e561db6be027fecdd8fea49267
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-27 22:35:12 -0300

    Adicionado children

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Container({ color, children }) {
   return (
     <div className={`w-100 bg-${color} py-20`}>
-
-    <div className="max-w-6xl mx-auto">
+      <div className="max-w-6xl mx-auto">
+        { children }
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
