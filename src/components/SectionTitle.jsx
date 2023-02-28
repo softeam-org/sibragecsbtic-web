1a7b52eecea83483e8ab1e142ed283bffed8a289
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-28 19:54:47 -0300

    Estilos do SectionTitle alterados

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SectionTitle({ title }) {
   return (
-    <div className="mb-6 py-4 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
+    <div className="mb-6 py-4 border-b-3 border-b-nord2 text-nord1 text-3xl font-bold">
           <h2>{ title }</h2>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   theme: {
     extend: {
       width:{'900': '900px'},
+      borderWidth: { '3': '3px' },
     },
   },
   plugins: [
