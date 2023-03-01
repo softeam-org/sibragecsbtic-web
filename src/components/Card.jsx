4bbd59f32de12f72f18a590ad720bfb980b4d2df
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-03-01 18:10:35 -0300

    Estilo do "card" alterado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function Card({image,name,description,institution, key}){
     return(
-        <div className="bg-white rounded-lg shadow-md text-gray-800 mx-8 mb-8 drop-shadow-md " >
+        <div className="bg-white rounded-lg shadow-md text-gray-800  drop-shadow-md h-60 " >
             <div className="flex justify-center">
             <img className="object-cover rounded-full w-24 h-24 mt-4" src={image} alt=""></img>
             </div>
 
-            <div className="p-4">
+            <div className="p-4 flex flex-col gap-3">
             <div className="flex justify-center">
-            <h3 className="text-lg font-semibold font-bold">{name}</h3>
+            <h3 className="sm:text-lg font-bold text-center">{name}</h3>
             </div>
-            <p>{description}</p>
+            {/* <p>{description}</p> */}
             
             <div className="flex justify-center font-bold">
             <h3>{institution}</h3>
