b1fc133398fbc63f37101baed68892b2a4a397e1
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-03-01 18:11:27 -0300

    Conteudo da "Home" atualizado

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -6,12 +6,25 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Leftarrow from '../components/Leftarrow';
 import Rightarrow from '../components/Rightarrow';
-import Luciano from '../assets/images/Luciano_IFS.jpg'
+import Luciano from '../assets/images/luciano.jpg'
 import logo from "../assets/logo-01.svg"
 import Reymard from "../assets/images/reymard.png"
 import Tatiana from "../assets/images/tatiana.jpeg"
 import Sergio from "../assets/images/sergio.jpg"
-
+import Regina from "../assets/images/regina.png"
+import Debora from "../assets/images/debora.jpg"
+import Herbert from "../assets/images/herbert.jpeg"
+import Eduardo from "../assets/images/eduardo.jpg"
+import Clarissa from "../assets/images/clarissa.png"
+import Ricardo from "../assets/images/ricardo.jpg"
+import Fernando from "../assets/images/fernando.jpeg"
+import Marcelo from "../assets/images/marcelo.jpg"
+import Emerson from "../assets/images/emerson.jpg"
+import Marco from "../assets/images/brasiel.jpg"
+import Caroline from "../assets/images/caroline.jpg"
+import Denise from "../assets/images/denise.jpg"
+import Mayana from "../assets/images/mayana.jpeg"
+import Rosana from "../assets/images/rosana.png"
 
 function Home() {
 
@@ -45,7 +58,7 @@ function Home() {
 
   const data = [{
     id:1,
-    name:"Prof. Dr. Reymard Sávio S. de Melo",
+    name:"Prof. Dr. Reymard Sávio Sampaio de Melo",
     image: Reymard,
     organization: "UFBA",
     function:"Scientific Committee",
@@ -61,6 +74,14 @@ function Home() {
   },
   {
     id:3,
+    name:"Prof. Dra. Regina Coeli Ruschel",
+    image:Regina,
+    organization: "UNICAMP",
+    function:"Scientific Committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:4,
     name:"Prof. Sérgio Leal Ferreira",
     image:Sergio,
     organization: "USP",
@@ -68,31 +89,131 @@ function Home() {
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
   },
   {
-    id:4,
-    name:"Teste4",
-    image:Luciano ,
-    funcao:"b",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  },
-  {
     id:5,
-    name:"Teste4",
-    image:Luciano ,
-    funcao:"b",
+    name:"Profª. Drª. Débora de Gois Santos",
+    image:Debora ,
+    organization: "UFS",
+    function:"Organizing committee",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
   },
   {
     id:6,
-    name:"Teste4",
-    image:Luciano ,
-    funcao:"b",
+    name:"Prof. Me. Herbert Melo Cruz",
+    image:Herbert ,
+    organization: "IFS",
+    function:"Organizing committee",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
   },
   {
     id:7,
-    name:"Teste4",
+    name:"Prof. Dr. Eduardo Toledo",
+    image:Eduardo ,
+    organization: "USP",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:8,
+    name:"Prof. Sérgio Leal Ferreira",
+    image:Sergio,
+    organization: "USP",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:9,
+    name:"Prof. Dra. Tatiana Gondim do Amaral",
+    image:Tatiana,
+    organization: "UFG",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:10,
+    name:"Profª. Drª. Clarissa Notariano Biotto",
+    image:Clarissa ,
+    organization: "UFC",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:11,
+    name:"Prof. Dr. Ricardo Juan José Oviedo Haito",
+    image:Ricardo ,
+    organization: "UFSC",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:12,
+    name:"Prof. Me. Luciano Silva Vasconcelos",
     image:Luciano ,
-    funcao:"b",
+    organization: "IFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:13,
+    name:"Prof. Dr. Fernando Márcio de Oliveira",
+    image:Fernando ,
+    organization: "UFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:14,
+    name:"Prof. Me. Marcelo Augusto Costa Maciel",
+    image:Marcelo ,
+    organization: "UFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:15,
+    name:"Prof. Me. Emerson Meireles de Carvalho",
+    image:Emerson ,
+    organization: "UFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:16,
+    name:"Prof. Dr. Marco Antônio Brasiel Sampaio",
+    image:Marco ,
+    organization: "UFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:17,
+    name:"Prof. Ma. Caroline Almeida Aragão Cabral",
+    image:Caroline ,
+    organization: "CBIM-SE",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:18,
+    name:"Profª. Drª. Denise Conceição de Gois Santos Michelan",
+    image:Denise ,
+    organization: "UFS",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:19,
+    name:"Profª. Me. Mayana Chagas Carvalho",
+    image:Mayana ,
+    organization: "IFPB",
+    function:"Organizing committee",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id:20,
+    name:"Rosana Dal Molin Inghes",
+    image:Rosana ,
+    organization: "ANTAC",
+    function:"Organizing committee",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
   }
 ]
@@ -123,16 +244,16 @@ function Home() {
   // console.log(data.map((valor)=>{ <Card key={valor.id} image = {valor.image} name={valor.name}/>}))
   return (
     <div className="bg-slate-50 text-nord0 ">
-      <div className='flex px-32 items-center'>
-        <div className=' flex '>
-        <div>
-        <h1 className='text-4xl font-bold pb-2 '>Indústria 5.0: Oportunidades e Desafios para Arquitetura e Construção</h1>
-        <h2 className=''>13º Simpósio Brasileiro de Gestão e Economia da Construção e 4º Simpósio Brasileiro de Tecnologia da Informação e Comunicação na Construção</h2>
+      <div className='flex px-6 sm:px-6 md:px-10 lg:px-32 items-center sm:gap-10'>
+        <div className=' flex flex-col sm:gap-2 '>
+      
+        <h1 className='text-xs sm:text-lg md:text-xl lg:text-4xl font-bold pb-2 '>Indústria 5.0: Oportunidades e Desafios para Arquitetura e Construção</h1>
+        <h2 className='text-xxs sm:text-sm'>13º Simpósio Brasileiro de Gestão e Economia da Construção e 4º Simpósio Brasileiro de Tecnologia da Informação e Comunicação na Construção</h2>
 
-        </div>
+        
         </div>
         <div>
-          <img src={logo} alt="logo" className="w-800 h-auto mx-auto"/>
+          <img src={logo} alt="logo" className="w-96 sm:w-800  h-auto mx-auto"/>
         </div>
       </div>
 
@@ -143,7 +264,7 @@ function Home() {
       </div>
       
 
-      <div className='text-base px-32 bg-gray-200 py-12'>
+      <div className='text-base px-6 sm:px-6 md:px-10 lg:px-32 bg-gray-200 py-12'>
         <h3 className='font-bold text-lg text-slate-900'>Sobre o evento</h3>
           <div className="my-4">
             <p>
@@ -184,22 +305,22 @@ function Home() {
           </div>
       </div>
       <div>
-      <h3 className='pt-4 px-32 font-bold text-lg text-slate-900'>Organização</h3>
-        <p className='py-4 px-32'>
+      <h3 className='pt-4 px-6 sm:px-6 md:px-10 lg:px-32 font-bold text-lg text-slate-900'>Organização</h3>
+        <p className='py-4 px-6 sm:px-6 md:px-10 lg:px-32'>
         De acordo com as normas da ANTAC para realização de eventos, o SIBRAGEC / SBTIC 2023 contará com um comitê científico, responsável pela programação e pela gestão, avaliação e aprovação final dos artigos submetidos, e uma comissão organizadora, responsável pela gestão e provimento dos recursos necessários para a realização do evento.
         </p>
 
       <div className='flex justify-center '>
-      <button disabled={isActive} onClick={handleClick} className='bg-transparent bg-none text-gray-400 disabled:text-gray-900'>
+      <button disabled={isActive} onClick={handleClick} className='bg-transparent duration-0 bg-none text-gray-400 outline-none hover:border-none focus:outline-none disabled:text-gray-900'>
         Comite Cientifico
       </button>
-      <button disabled={isActive2} onClick={handleClick} className='bg-transparent bg-none text-gray-400 disabled:text-gray-900'>
+      <button disabled={isActive2} onClick={handleClick} className='bg-transparent duration-0 bg-none text-gray-400 outline-none hover:border-none focus:outline-none disabled:text-gray-900'>
         Comissão Organizadora
       </button>
       </div>
       
       
-        <div className='px-32 py-8'>
+        <div className='px-6 sm:px-6 md:px-10 lg:px-32 py-8'>
 
         {isActive && 
         <Slider {...settings} className='flex justify-center' > 
@@ -212,7 +333,7 @@ function Home() {
 
       {isActive2 && 
       <Slider {...settings} className='flex justify-center' > 
-      {data.filter(data=>data.funcao==="b").map(data=>criarCard(data))}
+      {data.filter(data=>data.function==="Organizing committee").map(data=>criarCard(data))}
       </Slider>
       }
         </div>
