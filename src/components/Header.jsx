2e30358531b6076229c81860263455f51b35495b
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-02 23:11:29 -0300

    Corrigido bug de estilo hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../assets/logo-01.svg";
 import Navlinks from "./Navlinks";
@@ -8,10 +8,14 @@ import menu from "../assets/menuMobile.svg";
 
 
 function Header({ toggleFunction }) {
+  const [active, setActive] = React.useState(null);
 
   return (
     <header className="mb-20 lg:mb-0">
-      <div className="w-full bg-nord0 fixed top-0 lg:static">
+      <div 
+      className="w-full bg-nord0 fixed top-0 lg:static"
+      onMouseLeave={() => setActive(null)} 
+      >
       <div className="mx-auto max-w-6xl z-30 p-4 gap-20  lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
         <a href="/" className="w-48 h-20 md:w-60 flex justify-center items-center">
             <img src={logo2} alt="logo" className="w-60 absolute"/>
@@ -19,7 +23,7 @@ function Header({ toggleFunction }) {
         
         <div className="flex flex-col items-center gap-16">
           <div className="flex gap-20 pb-2 lg:pb-10 ">
-           <Navlinks />
+           <Navlinks active={active} setActive={setActive} />
           {
             <img onClick={toggleFunction} src={menu} className="lg:hidden" />
           }
