65c13da31d156653c734ed5919d4eb29e26cead5
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-03-03 08:29:57 -0300

    PlayerVideo atualizado

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function VideoPlayer(){
 
     return(
-        <div className="relative w-full h-0" style={{ paddingBottom: "56.25%" }}>
-            <iframe className="absolute top-0 left-0 w-full h-full" width="560" height="315" src="https://www.youtube.com/embed/ylm3gAoPXfA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <div className="flex justify-center">
+            <iframe className="" width="560" height="315" src="https://www.youtube.com/embed/ylm3gAoPXfA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
     )
 }
