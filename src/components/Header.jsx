daec4467f5786b60e2ee2ec4221a11d6ee742788
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-02 22:21:45 -0300

    Header fixado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ import menu from "../assets/menuMobile.svg";
 function Header({ toggleFunction }) {
 
   return (
-    <header className="bg-nord0">
-      <div className="max-w-6xl mx-auto p-4 lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
+    <header className="mb-20">
+      <div className="max-w-6xl mx-auto w-full z-30 bg-nord0 p-4 fixed top-0 lg:static lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
         
         <a href="/" className="relative lg:mx-auto w-48  h-20 md:w-60 truncate flex justify-center items-center">
             <img src={logo2} alt="logo" className="h-60 absolute"/>
@@ -20,9 +20,7 @@ function Header({ toggleFunction }) {
         <div className="flex flex-col items-center gap-16">
           <div className="flex gap-20 pb-2 lg:pb-10 ">
            <Navlinks />
-          {/*
-            sidebar button
-          */
+          {
             <img onClick={toggleFunction} src={menu} className="lg:hidden" />
           }
           </div>
