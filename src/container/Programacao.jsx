a3f5a8ca3a4aee03c064aa929c1f06ad15655880
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-04 13:54:56 -0300

    Página de construção criada

--- src/container/Programacao.jsx
+++ src/container/Programacao.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Schedule from "../components/Schedule";
 import Speakers from "../components/Speakers";
 import Container from "../components/Container";
+import Construction from "../components/Construction";
 
 function Programacao() {
 
   const [dia, setDia] = React.useState(1);
   const dias = [1, 2, 3];
 
+  return (<Construction />)
+  {/*
   return (
     <div className="bg-white text-nord0 pb-10">
       <Container color="nord6">
@@ -21,13 +24,13 @@ function Programacao() {
                 onClick={() => setDia(diaAtual)}
                 >
                 <p className="font-bold">Dia {diaAtual}</p>
-              </div>
+                </div>
             ))}
            
-          </div>
+            </div>
           <div className="flex w-full sm:w-2/5 lg:w-1/5">
-            <div className={`${dia >= 2 ? "bg-nord0" : "bg-zinc-300"} invisible sm:visible w-2/4 h-1 relative left-1 bottom-8 z-0`} />
-            <div className={`${dia >= 3 ? "bg-nord0" : "bg-zinc-300"} invisible sm:visible w-2/4 h-1 relative right-1 bottom-8 z-0`} />
+          <div className={`${dia >= 2 ? "bg-nord0" : "bg-zinc-300"} invisible sm:visible w-2/4 h-1 relative left-1 bottom-8 z-0`} />
+          <div className={`${dia >= 3 ? "bg-nord0" : "bg-zinc-300"} invisible sm:visible w-2/4 h-1 relative right-1 bottom-8 z-0`} />
           </div>
         </div>
       <Schedule day={dia} />
@@ -35,6 +38,7 @@ function Programacao() {
       <Speakers day={dia} />
     </div>
   );
+*/}
 }
 
 export default Programacao;
