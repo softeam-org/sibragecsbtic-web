e93a5a4da1104625613ac0d61e81c0ee34a5854e
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-04 14:12:54 -0300

    Header estilizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import logo from "../assets/logo-01.svg";
 import Navlinks from "./Navlinks";
 import logo2 from "../assets/logo-variacao-branca.svg";
 import menu from "../assets/menuMobile.svg";
+import { AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 
@@ -17,7 +18,7 @@ function Header({ toggleFunction }) {
       className="w-full bg-nord0 fixed z-50 top-0 lg:static"
       onMouseLeave={() => setActive(null)} 
       >
-      <div className="mx-auto max-w-6xl z-30 p-4 gap-20  lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
+      <div className="mx-auto max-w-6xl z-30 px-6 py-1 gap-20  lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
         <Link to="/" className="w-48 h-20 md:w-60 flex justify-center items-center">
             <img src={logo2} alt="logo" className="w-60 absolute"/>
         </Link>
@@ -25,9 +26,7 @@ function Header({ toggleFunction }) {
         <div className="flex flex-col items-center gap-16">
           <div className="flex gap-20 pb-2 lg:pb-10 ">
            <Navlinks active={active} setActive={setActive} />
-          {
-            <img onClick={toggleFunction} src={menu} className="lg:hidden" />
-          }
+            <AiOutlineMenu onClick={ toggleFunction } size={30} className="lg:hidden" />
           </div>
         </div>
           </div>
