8bd48218b1362c2cfe8c455c3511b2cb94b06858
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-04 15:33:06 -0300

    Ajuste de espaçamento

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SectionTitle({ title }) {
   return (
-    <div className="my-6 py-2 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
+    <div className="my-6 mb-10 py-2 border-b-2 border-b-nord2 text-nord1 text-3xl font-bold">
           <h2>{ title }</h2>
     </div>
   )
