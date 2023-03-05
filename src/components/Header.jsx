1349c1ec0637569e1938b0b39f369627f24b9446
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-05 12:13:57 -0300

    FIX:Corrigida cor no light mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,9 +13,9 @@ function Header({ toggleFunction }) {
   const [active, setActive] = React.useState(null);
 
   return (
-    <header className="mb-16 lg:mb-0">
+    <header className="mb-14 lg:mb-0">
       <div 
-      className="w-full bg-nord0 fixed z-30 top-0 lg:static"
+      className="w-full bg-nord0 fixed pb-1 z-30 top-0 lg:static"
       onMouseLeave={() => setActive(null)} 
       >
       <div className="mx-auto max-w-6xl z-30 px-6 gap-10  lg:p-0 lg:pt-4 flex lg:flex-col items-center justify-between lg:justify-start">
@@ -26,7 +26,7 @@ function Header({ toggleFunction }) {
         <div className="flex flex-col items-center gap-16">
           <div className="flex gap-20 pb-2 lg:pb-10 ">
            <Navlinks active={active} setActive={setActive} />
-            <AiOutlineMenu onClick={ toggleFunction } size={30} className="lg:hidden" />
+            <AiOutlineMenu color="white" onClick={ toggleFunction } size={30} className="lg:hidden" />
           </div>
         </div>
           </div>
