0c5bdd6605fa07027b42f54b26d24f5db053298a
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-05 10:52:46 -0300

    Alterado texto do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,21 +6,21 @@ function Footer() {
     <div >
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 bg-nord0 sm:px-8 lg:px-28 px-5 pt-12 gap-6 sm:gap-1'>
       <div className='flex flex-col gap-2'>
-        <h3 className='font-semibold'>Início</h3>
+        <h3 className='text-nord6 font-semibold'>Início</h3>
         <div className='flex flex-col'>
         <a className='text-sm text-stone-500'>Sobre</a>
         <a className='text-sm text-stone-500'>Organização</a>
         </div>
       </div>
       <div className='flex flex-col gap-2'>
-        <h3 className='font-semibold'>Programação</h3>
+        <h3 className='text-nord6 font-semibold'>Programação</h3>
         <div className='flex flex-col'>
         <a className='text-sm text-stone-500'>Horarios</a>
         <a className='text-sm text-stone-500'>Palestrantes do dia</a>
         </div>
       </div>
       <div className='flex flex-col gap-2'>
-        <h3 className='font-semibold'>Inscrição</h3>
+        <h3 className='text-nord6 font-semibold'>Inscrição</h3>
         <div className='flex flex-col'>
         <a className='text-sm text-stone-500'>Sobre</a>
         <a className='text-sm text-stone-500'>Preços</a>
@@ -28,7 +28,7 @@ function Footer() {
         </div>
       </div>
       <div className='flex flex-col gap-2'>
-        <h3 className='font-semibold'>Submissão</h3>
+        <h3 className='text-nord6 font-semibold'>Submissão</h3>
         <div className='flex flex-col'>
         <a className='text-sm text-stone-500'>Artigos</a>
         <a className='text-sm text-stone-500'>Calendario</a>
@@ -36,7 +36,7 @@ function Footer() {
         </div>
       </div>
       <div className='flex flex-col gap-2'>
-        <h3 className='font-semibold '>Local</h3>
+        <h3 className='text-nord6 font-semibold '>Local</h3>
         <div className='flex flex-col'>
         <a className='text-sm text-stone-500'>Hotel do evento</a>
         </div>
