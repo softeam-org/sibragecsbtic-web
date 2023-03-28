2e1e29bc497f0c78e6119789d05446cb6fef66ac
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-28 18:53:10 -0300

    página formatada

--- src/container/Inscricoes.jsx
+++ src/container/Inscricoes.jsx
@@ -11,33 +11,31 @@ import { Link } from "react-router-dom";
 import Construction from "../components/Construction";
 
 function Inscricoes() {
-return (
-  <div name="sobre" className="bg-white">
-  <Container color="nord6">
-  <SectionTitle title={"Inscrições"} />
+  return (
+    <div name="sobre" className="bg-white">
+      <Container color="nord6">
+        <SectionTitle title={"Inscrições"} />
         <div className="w-100 flex justify-center mb-10 mt-8">
           <h3 className=" text-nord1 text-2xl font-bold">Como se inscrever</h3>
-          </div>
-          <Instructions data={subscribeInstructions} />
-          
-          <Prices />
-          <div className="flex flex-col w-100 justify-center items-center gap-6">
+        </div>
+        <Instructions data={subscribeInstructions} />
+
+        <Prices />
+        <div className="flex flex-col w-100 justify-center items-center gap-6">
           <p className="text-nord-0 text-xl font-bold">
-          Clique no botão para se inscrever no evento
+            Clique no botão para se inscrever no evento
           </p>
           <Link to="/" rel="noreferrer">
-          <button className="bg-orange-500 border-0 text-white font-bold py-3 px-16  shadow-md hover:shadow-lg rounded-md hover:bg-orange-400 transition-all ease-in">
-          Inscreva-se
-          </button>
+            <button className="bg-orange-500 border-0 text-white font-bold py-3 px-16  shadow-md hover:shadow-lg rounded-md hover:bg-orange-400 transition-all ease-in">
+              Inscreva-se
+            </button>
           </Link>
-          </div>
-          </Container>
-          
-          <Courses />
-          </div>
-          );
+        </div>
+      </Container>
 
-        }
-        
-        export default Inscricoes;
-        
+      <Courses />
+    </div>
+  );
+}
+
+export default Inscricoes;
