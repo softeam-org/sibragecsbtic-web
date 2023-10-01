17a9f15b1a3665cde509b7ce0c941c6c0e386961
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-10-01 09:21:10 -0300

    constantes atualizadas

--- src/array.js
+++ src/array.js
@@ -36,6 +36,8 @@ import Agilean from "./assets/images/Agilean.png"
 import Climb from "./assets/images/Climb.png"
 import Cad from "./assets/images/Cad.png"
 import Capes from "./assets/images/capes.png"
+import Confea from "./assets/images/Confea.png"
+import Sippro from "./assets/images/Sippro.png"
 
 
 
@@ -297,7 +299,17 @@ export const logo = [
   },
   {
     id:19,
+    image: Sippro,
+    type: "apoio"
+  },
+  {
+    id:20,
     image: Capes,
     type: "patrocinio"
-  }  
+  },
+  {
+    id:21,
+    image: Confea,
+    type: "patrocinio"
+  }    
 ]
